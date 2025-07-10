78888c53407174294fd81029cad85db9f33bc021
xiaoyang <michstabe515@163.com>
2025-07-10 10:08:56 +0800

    update: 更新Vue,算法

--- 框架/Vue3/vue-basic/src/App.vue
+++ 框架/Vue3/vue-basic/src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive,shallowRef, watchEffect, triggerRef } from "vue";
+// const state = shallowRef({
+//   count: 0,
+//   user: { name: "Alice" },
+// });
+
+// watchEffect(() => {
+//   console.log(state.value);
+// });
+// state.value.count = 1;
+// triggerRef(state);
+// // state.value = {
+// //   count: 1,
+// //   user: { name: "Alice" },
+// // };
+// // state.value.count = 1;
+
+const info  = reactive({
+  a: 1
+})
+const 
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>{{ obj.a }}</div>
+  <div>{{ obj1 }}</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
